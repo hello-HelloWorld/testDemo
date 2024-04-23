f34799b50ec2386145d0915f72503db52f730cb0
xiong <591577295@qq.com>
2024-04-23 12:00:29 +0800

    no message

--- src/main/java/mianShi/ArrayMergeSort.java
+++ src/main/java/mianShi/ArrayMergeSort.java
@@ -1,4 +1,31 @@
 package mianShi;
 
+import java.util.Arrays;
+
 public class ArrayMergeSort {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 5, 7, 9};
+        int[] arr2 = {2, 4, 6, 8};
+        int[] merge = merge(arr1, arr1.length, arr2, arr2.length);
+        System.out.println(Arrays.toString(merge));
+    }
+
+    private static int[] merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = 0, p2 = 0;
+        int[] sorted = new int[m + n];
+        int cur;
+        while (p1 < m || p2 < n) {
+            if (p1 == m) {
+                cur = nums2[p2++];
+            } else if (p2 == n) {
+                cur = nums1[p1++];
+            } else if (nums1[p1] < nums2[p2]) {
+                cur = nums1[p1++];
+            } else {
+                cur = nums2[p2++];
+            }
+            sorted[p1 + p2 - 1] = cur;
+        }
+        return sorted;
+    }
 }
--- src/main/java/mianShi/CommonStr.java
+++ src/main/java/mianShi/CommonStr.java
@@ -1,4 +1,44 @@
 package mianShi;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CommonStr {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>(Arrays.asList("flower", "flowd", "floaf"));
+        String commonStr = getCom(list);
+        System.out.println(commonStr);
+
+        String[] strs = {"flower", "flowd", "floaf"};
+        String com = getCom2(strs);
+        System.out.println(com);
+    }
+
+    private static String getCom(ArrayList<String> list) {
+        String str1 = list.get(0);
+        for (int i =1;i< str1.length();  i++) {
+            String substr = str1.substring(0, i);
+            for (int j = 1; j < list.size(); j++) {
+                String str2 = list.get(j);
+                if (!str2.startsWith(substr)) {
+                    return str1.substring(0, i-1);
+                }
+            }
+        }
+        return "";
+    }
+
+    public static String getCom2(String[] strs) {
+        int len = strs[0].length();
+        int count = strs.length;
+        for (int i = 0; i < len; i++) {
+            char tmp = strs[0].charAt(i);//每次分别比对第一个单词的每一位
+            for (int j = 1; j < count; j++) {
+                if ((i == strs[j].length()) || (strs[j].charAt(i) != tmp)) {//若以后单词长度短于第一个单词长度或者相同位单词不同
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
 }
